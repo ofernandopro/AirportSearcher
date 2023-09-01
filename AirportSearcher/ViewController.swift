ceb4cb1e56230113ef45473797b776aaf8dc0fd4
fernando <fernando@lealapps.com>
2023-09-01 11:35:45 -0300

    Initial Commit

---
+++ AirportSearcher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AirportSearcher
+//
+//  Created by Fernando on 01/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
